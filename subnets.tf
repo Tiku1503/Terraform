2fe83ff5222f46bc581a3729995de75bdfeaf09e
tiku enyang <tikuenyang23@gmail.com>
2025-04-09 13:17:55 -0500

    correcting errors

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+  region = "us-east-1"  
 }
 
 resource "aws_vpc" "main_vpc" {
@@ -14,11 +14,11 @@ resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"  # Change as needed
+  availability_zone = "us-east-1a"  
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"  # Change as needed
+  availability_zone = "us-east-1b" 
 }
