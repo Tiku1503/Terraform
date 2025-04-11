6c60cf0466faedcb6a20a4c00d6405d4512c6ae9
Tiku1503 <tikuenyang23@gmail.com>
2025-04-11 11:10:36 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,17 +1,25 @@
 provider "aws" {
-  region = "us-east-1" 
-  access_key =$(( secrets.AWS_ACCESS_KEY )
-  secret_key =$(( secrets.AWS_SECRET_KEY )
+  region = "us-east-1"  
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
+
 terraform {
   backend "s3" {
     bucket = "forterraform1503"
     key    = "terraform/terraform.tfstate"
     region = "us-east-1"
-    access_key = $(( secrets.AWS_ACCESS_KEY )
-    secret_key = $(( secrets.AWS_SECRET_KEY )
-  }
-}
+    access_key = var.access_key
+    secret_key = var.secret_key
+    )
+
+    variable "access_key" {
+      type = string
+      
+    }
+    variable "secrets_key"
+      type = string
+    }
 
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -32,4 +40,3 @@ resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b" 
-}
