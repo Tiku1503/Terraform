b6ad9fbda35a179d71471241325e29d05859b31c
Tiku1503 <tikuenyang23@gmail.com>
2025-05-10 17:07:10 -0500

    changes

--- .github/vpc.tf
+++ .github/vpc.tf
@@ -1,14 +1,12 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 terraform {
   backend "s3" {
     bucket         = "forterraform1503"
     key            = "terraform/terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-1"
     access_key = "var.access_key"
     secret_key = "var.secret_key"
     
