6d88d8525cf22a48cb79f8046fedab4cd7c56c3a
Tiku1503 <tikuenyang23@gmail.com>
2025-05-10 18:08:55 -0500

    vpc init changes

--- .github/vpc.tf
+++ .github/vpc.tf
@@ -7,25 +7,26 @@ terraform {
     bucket         = "forterraform1503"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-2"
-    access_key = "var.access_key"
-    secret_key = "var.secret_key"
       
     }
   }
 
-resource "aws_internet_gateway" "main_igw" {
-  vpc_id = aws_vpc.main_vpc.id
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
+
+
+data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a" 
+  availability_zone       = "us-east-1a"
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b" 
+  availability_zone = "us-east-1b"
 }
